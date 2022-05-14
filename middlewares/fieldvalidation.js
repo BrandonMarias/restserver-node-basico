46586f9a03ec5e86ad4129495448e8fc506d3ada
BrandonMarias <80177652+BrandonMarias@users.noreply.github.com>
2022-05-14 09:36:41 -0500

    error en el servidor

--- middlewares/fieldvalidation.js
+++ middlewares/fieldvalidation.js
@@ -1,4 +1,4 @@
-const {validationResult} = require('express-validator')
+const { validationResult } = require("express-validator");
 
 const fieldValidation = (req, res, next) => {
   const errors = validationResult(req);
