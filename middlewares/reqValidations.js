22772da7feebd0935431e3f6bee0c0c3b7bcae32
BrandonMarias <80177652+BrandonMarias@users.noreply.github.com>
2022-05-14 10:15:22 -0500

    reqvalidatios

--- middlewares/reqValidations.js
+++ middlewares/reqValidations.js
@@ -1,6 +1,6 @@
 const { validationResult } = require("express-validator");
 
-const fieldValidation = (req, res, next) => {
+const reqValidations = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors);
@@ -9,4 +9,4 @@ const fieldValidation = (req, res, next) => {
   next();
 };
 
-module.exports = { fieldValidation };
+module.exports = { reqValidations };
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,17 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 const { check } = require("express-validator");
-
-const { validationResult } = require("express-validator");
-
-const  fieldValidation  = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors);
-  }
-
-  next();
-};
+const {reqValidations} = require('../middlewares/reqValidations')
 
 const {
   validateRole,
@@ -31,7 +21,7 @@ router.get("/", getUsers);
 
 router.put("/:id", [
   check('id', 'ID invalido').isMongoId().custom(valodateUserId),
-  fieldValidation
+  reqValidations
 ], putUsers);
 
 router.post(
@@ -42,14 +32,14 @@ router.post(
     check("password", "contraseñ no aceptable").isLength({ min: 8 }),
     // check('role', 'role no valido').isIn(["ADMIN_ROLE", "USER_ROLE"]),
     check("role").custom(validateRole),
-    fieldValidation,
+    reqValidations,
   ],
   postUsers
 );
 
 router.delete("/:id", [
   check('id', 'ID invalido').isMongoId().custom(valodateUserId),
-  fieldValidation
+  reqValidations
 ] , deleteUsers);
 
 router.patch("/", patchUsers);
